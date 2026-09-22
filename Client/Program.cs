d30837a99ae5446ba074cc4fa21852e733ce08fa
luckpp <marius.andritoi@yahoo.com>
2026-09-22 20:50:24 +0300

    added dummy service and client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Dummy;
+using Grpc.Core;
+
+namespace LkpClient;
+
+class Program
+{
+    const string Target = "localhost:50051";
+
+    static void Main(string[] args)
+    {
+        Channel channel = new Channel(Target, ChannelCredentials.Insecure);
+
+        channel.ConnectAsync().ContinueWith(task =>
+        {
+            if (task.Status == TaskStatus.RanToCompletion)
+            {
+                Console.WriteLine("Connected to the server.");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to connect to the server: {task.Exception?.Message}");
+            }
+        }).Wait();
+
+        var client = new DummyService.DummyServiceClient(channel);
+
+        channel.ShutdownAsync().Wait();
+
+        Console.ReadKey();
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Grpc.Core;
+
+namespace LkpServer;
+
+class Program
+{
+    const int Port = 50051;
+
+    static void Main(string[] args)
+    {
+        Server server = null;
+
+        try
+        {
+            server = new Server
+            {
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+            Console.WriteLine("The server is listening on port " + Port);
+            Console.ReadKey();
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"Error starting server: {e.Message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Unexpected error: {e.Message}");
+        }
+        finally
+        {
+            server?.ShutdownAsync().Wait();
+        }
+    }
+}
